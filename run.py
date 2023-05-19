cecc1ee38db76f2da3b776f505bdd4902fa1ba3a
Joy M. A. Salchert <lapsed_argent.0t@icloud.com>
2023-05-19 18:53:26 +0000

    add ticket_price_index for first movie 'when harry met sally'

---
+++ run.py
@@ -0,0 +1,25 @@
+# index holding the movie names, age ratings, screening time, viewer age options and corresponding ticket prices
+ticket_price_index = {
+    'When Harry met Sally': {
+        'name' : 'When Harry met Sally',
+        'rating' : 0,
+
+        'times' : {
+            'morning' : {
+                'child' : 10,
+                'adult' : 16,
+                'senior' : 12
+            },
+            'afternoon' : {
+                'child' : 10,
+                'adult' : 18,
+                'senior' : 12
+            },
+            'evening' : {
+                'child' : 12,
+                'adult' : 20,
+                'senior' : 14
+            }
+        }
+    }
+}
